f666ae698d0f8b4ef53471644c24bf5ed5fca6a5
Dhyman Choudhury <dhyman2029@gmail.com>
2025-06-20 21:46:19 +0600

    error page complted

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import Lottie from 'lottie-react';
+import React from 'react';
+import error from '../assets/json/error.json'
+
+const ErrorPage = () => {
+    return (
+        <div className='mx-auto bg-[#0F172A]'>
+            <Lottie className='w-1/2 mx-auto' animationData={error} loop={true} />
+        </div>
+    );
+};
+
+export default ErrorPage;
