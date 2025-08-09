332ba8aab36c8ada78e4bfca58fb833edbe574cb
Dhyman Choudhury <dhyman2029@gmail.com>
2025-08-10 02:05:46 +0600

    made banner section responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from "motion/react";
 import banner from '../assets/images/banner-1.png'
 import image1 from '../assets/images/ai-1.avif'
 import image2 from '../assets/images/ai-2.jpg'
 
 const Banner = () => {
+   const [xValues, setXValues] = useState([100, 150, 100]);
+
+  useEffect(() => {
+    function updateXValues() {
+      const width = window.innerWidth;
+      if (width < 768) {
+        // Small devices below md
+        setXValues([70, 130, 70]);
+      } else if (width >= 768 && width < 1024) {
+        // md devices (768px - 1023px)
+        setXValues([80, 140, 80]);
+      } else {
+        // Large devices lg+
+        setXValues([100, 150, 100]);
+      }
+    }
+
+    updateXValues();
+    window.addEventListener('resize', updateXValues);
+
+    return () => window.removeEventListener('resize', updateXValues);
+  }, []);
     return (
        <div className='hero w-full  mx-auto min-h-96 '>
-        <div className='pb-5 w-full flex flex-col lg:flex-row-reverse gap-10' 
+        <div className='pb-5 w-full flex flex-col items-center justify-center lg:flex-row-reverse  gap-10' 
           style={{
                 backgroundImage: `url(${banner})`,
                 backgroundSize: 'cover',
@@ -20,19 +42,19 @@ const Banner = () => {
           }}>
             
 
-        <div className='flex-1'>
+        <div className='flex-1 ml-5 flex flex-col md:items-center lg:items-start lg:ml-10'>
           <motion.img
           src={image1}
           animate={{y: [100, 150, 100]}}
           transition={{duration: 5, repeat: Infinity}}
-          className='max-w-sm border-blue-800 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl'
+          className='max-w-[57%] border-blue-800 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl'
 
           />
           <motion.img
           src={image2}
-          animate={{x: [100, 150, 100]}}
+          animate={{x: xValues}}
           transition={{duration: 5, delay: 2, repeat: Infinity}}
-          className='max-w-sm border-blue-800 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl'
+          className='max-w-[57%] border-blue-800 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl'
           />
           
         </div>
